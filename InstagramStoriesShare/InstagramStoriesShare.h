82afdbd11cc43149c03d3f41aad5fc894e34ab5f
Tomosuke Okada <orenoti@gmail.com>
2019-10-16 00:31:13 +0900

    Create Project.

---
+++ InstagramStoriesShare/InstagramStoriesShare.h
@@ -0,0 +1,19 @@
+//
+//  InstagramStoriesShare.h
+//  InstagramStoriesShare
+//
+//  Created by Tomosuke Okada on 2019/10/16.
+//  Copyright © 2019 TomosukeOkada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstagramStoriesShare.
+FOUNDATION_EXPORT double InstagramStoriesShareVersionNumber;
+
+//! Project version string for InstagramStoriesShare.
+FOUNDATION_EXPORT const unsigned char InstagramStoriesShareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstagramStoriesShare/PublicHeader.h>
+
+
